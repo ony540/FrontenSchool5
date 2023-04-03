a41d3a02f193584a3a812ef8542b6da1fbba33f4
“ony540” <onyy540@gmail.com>
2023-04-03 23:38:42 +0900

    20230403 수업내용 확인 및 복습

--- JAVASCRIPT/20230403/016_undefined.js
+++ JAVASCRIPT/20230403/016_undefined.js
@@ -1,17 +1,17 @@
 // undefined은 트루인지 폴스인지 모르기때문에 조건문이 실행되지않는다.
-if(undefined){
+if (undefined) {
     console.log('hello')
-}//언디파인 - 실행되지않음 
+} //언디파인 - 실행되지않음 
 
 let aa;
-if (aa){
+if (aa) {
     console.log('hello')
 } //언디파인  - 실행되지않음 
 
 // 답할 수 있어야 견고한 코드를 짤 수 있습니다.
 // 견고한 코드 : 어떤 상황에서도 기능이 무너지지 않는 코드
 let aaa;
-if (aaa > 100){
+if (aaa > 100) {
     console.log('hello')
 } //undefined - 실행되지않음 
 
@@ -19,18 +19,20 @@ if (aaa > 100){
 let a;
 console.log(a);
 
+
 if (a) {
     // 실행안됨
     console.log('a에 아무것도 할당되지 않았습니다.')
-  }
+}
 
 if (typeof a === "undefined") {
     // a가 정의되지 않은 경우에 실행
     console.log('a에 아무것도 할당되지 않았습니다.')
-  }
+}
 
-  //위의 코드를 이렇게 줄일 수 있다!
+// a = 10; -> 이렇게 정의되어있으면 실행되지않음
+//위의 코드를 이렇게 줄일 수 있다!
 if (!a) {
-    // 실행된다 
+    // a가 정의되지 않은 경우에 실행
     console.log('a에 아무것도 할당되지 않았습니다.')
-  }
+}
--- JAVASCRIPT/20230403/017_null.js
+++ JAVASCRIPT/20230403/017_null.js
@@ -3,3 +3,25 @@ let value1; // undefined
 //존재하지만 값이나 자료형에 존제하지않는 foo
 var value2 = null;
 console.log(value2); //null
+
+// 참고삼아서만 알아주세요.
+// let 반점수 = [10, 20, 30, null, 40, 50]
+// let 반점수합 = 0
+// for (const i of 반점수) {
+//     console.log(i)
+//     반점수합 += i
+// }
+// console.log(반점수합)
+
+// 점수합 - 계산 가능(150)
+
+
+let 반점수 = [10, 20, 30, undefined, 40, 50]
+let 반점수합 = 0
+for (const i of 반점수) {
+    console.log(i)
+    반점수합 += i
+}
+console.log(반점수합)
+
+// 반평균 undefined가 하나라도 있으면 합의 결과값이 NaN!
