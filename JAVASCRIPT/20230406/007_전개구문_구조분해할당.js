7f674faeaa178c2c0f5e4e81f715b30f0ffcaf5a
“ony540” <onyy540@gmail.com>
2023-04-19 16:14:52 +0900

    20230419 수업내용

--- JAVASCRIPT/20230406/007_전개구문_구조분해할당.js
+++ JAVASCRIPT/20230406/007_전개구문_구조분해할당.js
@@ -5,15 +5,15 @@ const 생선들 = ['조기', '갈치', '다금바리'];
 const 합치면 = [...과일들, ...생선들];
 const 합치면2 = [과일들, 생선들];
 
-console.log(합치면);
-console.log(합치면2);
+console.log(합치면); //['사과', '파인애플', '수박', '조기', '갈치', '다금바리']
+console.log(합치면2); //[Array(3), Array(3)]
 
 // 객체의 전개 구문
 const 위니브1 = { 개리: 1, 빙키: 2 };
 const 위니브2 = { 라이캣: 3 };
 const 위니브3 = { ...위니브1, ...위니브2 };
 
-console.log(위니브3);
+console.log(위니브3); //{개리: 1, 빙키: 2, 라이캣: 3}
 
 
 const 위니브임직원 = {
@@ -34,7 +34,8 @@ const 위니브임직원3 = {
     ...위니브임직원
 }
 
-위니브임직원
+위니브임직원2 //{개리: '재직중', 빙키: '재직중', 라이켓: '휴가', 뮤라: '재직중', 해골왕: '퇴사'}
+위니브임직원3
 
 
 // 구조분해할당
@@ -54,10 +55,14 @@ Object.entries(위니브임직원)
 for (const [i, j] of Object.entries(위니브임직원)) {
     console.log(i, j);
 }
+for (const i of Object.entries(위니브임직원)) {
+    console.log(i); // ['개리', '재직중'] 배열형식
+}
 
 let [a, b] = [10, 20]
 let [one, two, three] = '010-5044-2903'.split('-')
 let [one, two, ...three] = '010-5044-2903-123-123'.split('-')
+//three ['2903', '123', '123']
 
 //////////////////////////////
 
@@ -67,7 +72,7 @@ const test1 = [
     [5, 6, [50, 60]]
 ]
 for (const [i, j] of test1) {
-    console.log(i, j);
+    console.log(i, j); //앞에 두개 1 2 만 출력
 }
 
 const test1 = [
@@ -76,7 +81,7 @@ const test1 = [
     [5, 6, [50, 60]]
 ]
 for (const [i, j, k] of test1) {
-    console.log(i, j, k);
+    console.log(i, j, k); //k에 [10, 20]
 }
 
 const test1 = [
@@ -86,7 +91,7 @@ const test1 = [
 ]
 // i, j, k, l, m, n
 for (const [i, j, [k, l]] of test1) {
-    console.log(i, j, k, l);
+    console.log(i, j, k, l); //1 2 10 20 
 }
 
 // 이것은 기억해주세요.
@@ -96,14 +101,14 @@ const test1 = [
     [5, 6, 50, 60]
 ]
 for (const [i, j, ...k] of test1) {
-    console.log(i, j, k);
+    console.log(i, j, k); //...k 했기때문에 뒤에있던 값이 모두 배열로 들어감 1 2 [10, 20]
 }
 
 function hello(a, b, ...c) {
     console.log(a, b, c)
 }
 
-hello(1, 2, 3, 4, 5, 6, 7)
+hello(1, 2, 3, 4, 5, 6, 7) //1 2 [3, 4, 5, 6, 7]
 
 //4월12일 추가
 Math.max(...[10,20,30])
@@ -117,6 +122,6 @@ Math.max([10,20,30,[11,12,13,[29,40,50]]].flat(Infinity))
 
 //자주 사용되는 코드
 const s = 'hello world';
-[s] // s.split() ['hello world']
+[s] // ['hello world']
 [...s] // s.split('') ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
 // {...s} 이렇게 전개할 이유는 없습니다. 잊으셔도 됩니다.
