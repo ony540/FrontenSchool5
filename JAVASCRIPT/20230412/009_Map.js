683eeb34653fa77b7bc369965783f82a08b3cc3a
“ony540” <onyy540@gmail.com>
2023-04-21 18:02:40 +0900

    Map, call by 복습

--- JAVASCRIPT/20230412/009_Map.js
+++ JAVASCRIPT/20230412/009_Map.js
@@ -2,13 +2,14 @@
 
 let m = new Map();
 
-//Map에 값을 넣기
+//Map에 값을 넣기 - .set 매서드 사용하기
+//문자열, 숫자, 불린값 모두 키가 될 수 있음 (배열이나 객체는 키가 될수는 있지만 변수에 넣어서 설정해야함!! 그 자체로는 X)
 m.set('하나', 1) //키와 벨류
 m.set(1,'하나')
 m.set(true,1)
 m.set(false,0)
 
-//Map의 키로 값에 접근하기
+//Map의 키로 값에 접근하기 - get()
 m.get('하나') // 1
 m.get(1) // '하나'
 
@@ -17,23 +18,23 @@ m.get(1) // '하나'
 
 [1,2,3] == [1,2,3] //false래 새로운 배열이다! 메모리주소값이 다름
 m.set([1,2,3],'리얼리?')
-m.get([1,2,3]) //오류 발생 위의 것이 다름!!
+m.get([1,2,3]) //undefined 여기서 [1,2,3]은 각각 다른 것을 의미함
 
 let x = [1,2,3]
 m.set(x,'리얼리?')
 m.get(x) //실행됨 x는 같은 것!!
 //----------
 
-// Map의 값이 있는지 확인하기
+// Map의 값이 있는지 확인하기 - .has() true
 console.log(m.has('하나'));
 
 
-// Map의 값을 제거하기
+// Map의 값을 제거하기 - .delete()
 console.log(m.delete('하나'));
 console.log(m.has('하나'));
 console.log(m);
 
-// Map의 크기를 확인하기
+// Map의 크기를 확인하기 .size
 console.log(m.size);
 
 
@@ -46,10 +47,16 @@ const data = new Map()
 
 ////////////
 
-//iterable한 객체로 쌍으로 이루어져있어야한다. 
+//iterable한 객체로 쌍으로 이루어져있어야한다. -> map으로 변형가능
+
 let data = new Map([['one', 1], ['two',2]]) // O
+
 //Object.entries ->  key, value 형태의 배열로 변환되어 출력됩니다.
 let data = new Map(Object.entries({'one':1, 'two':2})) // O
+
+//객체, 문자열, 배열은 map으로 바로 변환 불가 
+//-> 객체는 iterable한 객체가 아니다! ! !
+
 let data = new Map({'one':1, 'two':2}) // X
 // let data = new Map('hello world') // X
 // let data = new Map([10, 20, 30, 40]) // X
@@ -63,12 +70,12 @@ for (const i of data) { // X 작동되지 않습니다!
 }
 
 let data = {'one':1, 'two':2}
-for (const i of Object.entries(data)) {
+for (const i of Object.entries(data)) { 
     console.log(i)
 }
 
 
-// 직접 순회가 가능한 Map!
+// 직접 순회가 가능한 Map! for of 사용 가능
 let m = new Map();
 
 m.set('하나', 1) // 메서드 체이닝
@@ -77,12 +84,19 @@ m.set('하나', 1) // 메서드 체이닝
     .set('넷', 4)
 
 for (const i of m) {
-    console.log(i)
+    console.log(i) //['하나',1] 키와 값을 배열형식으로 반환
 }
 
 for (const [key,value] of m) {
     console.log(key,value)
 }
+
+
+// Map의 값에 접근 - MapIterator로 반환
+console.log(m.keys()); //MapIterator{'하나', '둘', '셋', '넷'}
+console.log(m.values());
+console.log(m.entries()); //MapIterator {'하나' => 1, '둘' => 2, '셋' => 3, '넷' => 4}
+
 
 
 //object의 단점
